b69c588a82781a1ff378f77721a9162e84925355
Rehman Akbar <rana496@gmail.com>
2019-03-05 18:02:38 +0500

    Swagger Api Docs

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -8,15 +8,112 @@ use App\User;
 use Illuminate\Support\Facades\Auth;
 use Validator;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="L5 OpenApi",
+ *      description="L5 Swagger OpenApi description",
+ *      @OA\Contact(
+ *          email="darius@matulionis.lt"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
+
+ /**
+ *  @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="L5 Swagger OpenApi dynamic host server"
+ *  )
+ *
+ *  @OA\Server(
+*      url="https://projects.dev/api/v1",
+ *      description="L5 Swagger OpenApi Server"
+ * )
+ */
+/**
+ * @OA\SecurityScheme(
+ *     type="oauth2",
+ *     description="Use a global client_id / client_secret and your username / password combo to obtain a token",
+ *     name="Password Based",
+ *     in="header",
+ *     scheme="https",
+ *     securityScheme="Password Based",
+ *     @OA\Flow(
+ *         flow="password",
+ *         authorizationUrl="/oauth/authorize",
+ *         tokenUrl="/oauth/token",
+ *         refreshUrl="/oauth/token/refresh",
+ *         scopes={}
+ *     )
+ * )
+ */
+/**
+ * @OA\Tag(
+ *     name="project",
+ *     description="Everything about your Projects",
+ *     @OA\ExternalDocumentation(
+ *         description="Find out more",
+ *         url="http://swagger.io"
+ *     )
+ * )
+ *
+ * @OA\Tag(
+ *     name="user",
+ *     description="Operations about user",
+ *     @OA\ExternalDocumentation(
+ *         description="Find out more about",
+ *         url="http://swagger.io"
+ *     )
+ * )
+ * @OA\ExternalDocumentation(
+ *     description="Find out more about Swagger",
+ *     url="http://swagger.io"
+ * )
+ */
+
 class UserController extends Controller
 {
     public $successStatus = 200;
 
-/**
+    /**
      * login api
      *
      * @return \Illuminate\Http\Response
      */
+
+     /**
+ * @OA\Post(
+ *      path="/login",
+ *      operationId="getProjectsList",
+ *      tags={"Login"},
+ *      summary="Login user",
+ *      description="Returns api token",
+ *      @OA\Parameter(
+ *          name="email",
+ *          description="user email",
+ *          required=true,
+ *          in="path",
+ *          @OA\Schema(
+ *              type="string"
+ *          ),
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="success"
+ *       ),
+ *       @OA\Response(response=401, description="Unauthorised"),
+ *       security={
+ *           {"api_key_security_example": {}}
+ *       }
+ *     )
+ *
+ * Returns list of projects
+ */
+
     public function login(){
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
